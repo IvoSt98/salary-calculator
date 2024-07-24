267c479acdbda7dd292847f3f99b487854b5018e
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-07-24 18:42:57 +0000

    Change in print statements in def get_income(),add 2 new defs to make the income in % and after that to make it in a table

--- run.py
+++ run.py
@@ -2,22 +2,24 @@ import pandas as pd
 from tabulate import tabulate
 
 def start():
-    """ the function expalin how the program it's working and for what it is.
+
+    """ The function expalin how the program it's working and for what it is.
     """
    
-    print("Welcome to Your Salary/Income Calculator!\n")
-    print("The 40/40/20 System:\n")
-    print("First, enter your income, which will be divided as follows:")
-    print("40 percentages - needs, 40 percentages - savings/investments, and 20 percentages - living expenses.")
-    print("Next, add your expenses by selecting a category and entering the purpose and amount. ")
-    print("In the end, you'll know if you're managing your finances well.\n")
-    print("Good Luck!/n")
-    print("Press enter to start")
+    print("     Welcome to Your Salary/Income Calculator!\n     ")
+    print("     The 40/40/20 System:\n      ")
+    print(" First, enter your income for a month, which will be divided as follows: ")
+    print(" 40 percentages - needs, 40 percentages - savings/investments, and 20 percentages - living expenses.\n   ")
+    print("-Example for NEEDS: this is your insuarance for the car, bills, credits, food and etc. The things without you can't live. ")
+    print("-Example for SAVINGS/INVESTMENTS: this is where you will write the money for education, for your holiday, investments in stocks and etc.")
+    print("-Example for LIVING EXPENSES: here you need to fill your money for cigaretes, parties, restaurants and etc. Something without you can survive\n")
+    print(" Next, add your expenses by selecting a category and entering the purpose and amount. ")
+    print(" In the end, you'll know if you're managing your finances well.\n ")
+    print("     Good Luck!\n        ")
     
-start()
 
 def get_income():
-
+    
     """ The function calculate the sum to be always float, if it's a string it'll be 
     displayed error, if it's less then 4 digits it will print again error message"""
 
@@ -31,8 +33,28 @@ def get_income():
                 print("Income must be at least 4 digits. Please try again.")
         except ValueError:
             print("Invalid data. Please enter a number with at least 4 digits.")
-            
-get_income()
+     
+def calculate_and_input_expense(income):
 
+    """ The def calculate the month income in % for needs, savings
+    and living"""
 
+    needs = 0.4 * income
+    savings = 0.4 * income
+    living = 0.2 * income
+    return needs, savings, living
+
+def build_table(needs, savings, living):
+
+    """ The def add the data from def calculate_and_input_expense() and make
+    it in a table. """
+    
+    table = ["Needs", "Savings/Investments", "Living Expenses"]
+    info_income = [[f"${needs}", f"${savings}", f"${living}"]]
+    print(tabulate(info_income, headers=table, tablefmt="simple"))
+
+start()
+income = get_income()
+needs, savings, living = calculate_and_input_expense(income)
+build_table(needs, savings, living)
 
