3f3e2fab067b3bf482a02e38079a34bda74dfdc5
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-07-24 15:27:08 +0000

    Add function start with few print statements which explain how and for what it's the program

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 import pandas as pd
 from tabulate import tabulate
 
+def start():
+    """ the function expalin how the program it's working and for what it is.
+    """
+
+    print("Welcome to Your Salary/Income Calculator!\n")
+    print("The 40/40/20 System:\n")
+    print("First, enter your income, which will be divided as follows:")
+    print("40 percentages - needs, 40 percentages - savings/investments, and 20 percentages - living expenses.")
+    print("Next, add your expenses by selecting a category and entering the purpose and amount. ")
+    print("In the end, you'll know if you're managing your finances well.\n")
+    print("Good Luck!/n")
+    print("Press enter to start")
+
+start()
