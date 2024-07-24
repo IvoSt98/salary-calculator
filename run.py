2c003fc3cb1bb9e66377552484eff0b5da05aded
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-07-24 16:37:02 +0000

    Add function get_income which ask the user for their income if the value it's not valid it will show an error and will ask the user untill the value it's not correct

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from tabulate import tabulate
 def start():
     """ the function expalin how the program it's working and for what it is.
     """
-
+   
     print("Welcome to Your Salary/Income Calculator!\n")
     print("The 40/40/20 System:\n")
     print("First, enter your income, which will be divided as follows:")
@@ -13,5 +13,26 @@ def start():
     print("In the end, you'll know if you're managing your finances well.\n")
     print("Good Luck!/n")
     print("Press enter to start")
-
+    
 start()
+
+def get_income():
+
+    """ The function calculate the sum to be always float, if it's a string it'll be 
+    displayed error, if it's less then 4 digits it will print again error message"""
+
+    while True: 
+        try:
+            user_income = input("Enter your income (at least 4 digits): ")
+            income = float(user_income)
+            if income >= 1000:  
+                return income
+            else:
+                print("Income must be at least 4 digits. Please try again.")
+        except ValueError:
+            print("Invalid data. Please enter a number with at least 4 digits.")
+            
+get_income()
+
+
+
